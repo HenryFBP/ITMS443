74af2948741b6986ca2ac45e76efab7a4e7faf98
HenryFBP <HenryFBP@gmail.com>
2018-11-17 14:28:32 -0500

    add wget and grep for <input>/<form> elts

---
+++ final/scripts/wget.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+require("./shared.pm");
+our(@TARGET, @OUTFOLDER, @SITES);
+
+# Download site contents.
+foreach my $site (@SITES) {
+	system("wget -r --no-clobber --directory-prefix=@OUTFOLDER/wget --no-parent $site");
+}
+
+# Find form elements.
+system("egrep '(<form)|(<input)' -r '@OUTFOLDER/wget'  --color=always 2>&1 | tee @OUTFOLDER/wget/forms_and_inputs.txt")
+
