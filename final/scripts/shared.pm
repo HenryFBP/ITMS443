1e98a6c0b45d5848e3515e19248898f4fe4d585b
HenryFBP <HenryFBP@gmail.com>
2018-11-20 21:51:26 -0500

    fix wrong ip

--- final/scripts/shared.pm
+++ final/scripts/shared.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 our @OUTFOLDER="out";
-our @TARGET="172.43.0.154"; # Hello there, random GitHub user. This is an internal IP. Just letting you know.
+our @TARGET="172.43.35.10"; # Hello there, random GitHub user. This is an internal IP. Just letting you know.
 
 our @SITE1="www.goodshopping.com"; # And these are also internal.
 our @SITE2="www.moviescope.com";
